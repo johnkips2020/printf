a4b0f787c267dea7782b468b9b12fe9eb752b9ff
John Kipngeno <johnkips2002@gmail.com>
2022-08-08 17:56:49 +0300

    printf

--- main.h
+++ main.h
@@ -1,24 +1,49 @@
-#ifndef our_printf
-#define our_printf
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+
+
+
 /**
- * struct specifier - struct specifier
- * @valid: the valid character.
- * @f: the functions associated.
+ * struct format - match the conversion specifiers for printf
+ * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
+ * @f: type pointer to function for the conversion specifier
  *
  */
-typedef struct specifier
+
+typedef struct format
 {
-	char *valid;
-	int (*f)(va_list);
-} spec;
-int _printf(const char *format, ...);
-int print_c(va_list args);
-int print_s(va_list args);
-int print_d(va_list args);
-int print_i(va_list args);
+	char *id;
+	int (*f)();
+} convert_match;
+
+int printf_pointer(va_list val);
+int printf_hex_aux(unsigned long int num);
+int printf_HEX_aux(unsigned int num);
+int printf_exclusive_string(va_list val);
+int printf_HEX(va_list val);
+int printf_hex(va_list val);
+int printf_oct(va_list val);
+int printf_unsigned(va_list args);
+int printf_bin(va_list val);
+int printf_srev(va_list args);
+int printf_rot13(va_list args);
+int printf_int(va_list args);
+int printf_dec(va_list args);
+int _strlen(char *s);
+int *_strcpy(char *dest, char *src);
+int _strlenc(const char *s);
+int rev_string(char *s);
+int _strlenc(const char *s);
+int printf_37(void);
+int printf_char(va_list val);
+int printf_string(va_list val);
 int _putchar(char c);
-int print_percent(va_list args);
-int (*get_func(char x))(va_list args);
+int _printf(const char *format, ...);
+
 #endif
